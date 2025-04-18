9f6892712630ddc7ed9a8bb12ef98623d47d5f37
davidrodriguez88 <davidxrodriguez@me.com>
2025-04-18 11:33:00 -0400

    Completed exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine()?.ToLower();
+
+            switch (favoriteSubject)
+            {
+                case "math":
+                    Console.WriteLine("Math is cool because it's logic-based!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is cool because of Bill Nye The Science Guy!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is very important assuming it's not rewritten and we can learn from it.");
+                    break;
+                case "writing":
+                    Console.WriteLine("Writing is rad!");
+                    break;
+                case "lunch":
+                    Console.WriteLine("One of my favorite subjects!");
+                    break;
+                default:
+                    Console.WriteLine("Interesting choice! What do you like about it?");
+                    break;
+            }
+            
         }
+        
     }
 }
